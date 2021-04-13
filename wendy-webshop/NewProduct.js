5f3f99a05a931617f9bbdfec1d0fa9e2afedf4c8
EnikoSstyn <35663980+EnikoSstyn@users.noreply.github.com>
2021-04-13 21:59:53 +0200

    made the basic new product page

---
+++ wendy-webshop/NewProduct.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function NewProduct() {
+    return (
+        <div>
+            <h1>Új termék hozzáadása</h1>
+            <form >
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" className="form-control mb-3" id="name"/>
+                <label htmlFor="description" className="form-label">Description</label>
+                <textarea id="description" className="form-control mb-3" cols="30" rows="10"></textarea>
+                <label htmlFor="price" className="form-label">Price</label>
+                <input type="number" className="form-control mb-3" id="price"/>
+                <label htmlFor="quantityOfStock" className="form-label">Quantity Of Stock</label>
+                <input type="range" className="form-range" id="quantityOfStock" min="0" max="100"/>
+            </form>
+        </div>
+    )
+}
