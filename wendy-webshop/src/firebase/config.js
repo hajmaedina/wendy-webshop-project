593f15d27426cce48442c3721be370f0d6300a48
Rita W <weszelovszkirita@gmail.com>
2021-04-13 17:40:40 +0200

    firebase config

---
+++ wendy-webshop/src/firebase/config.js
@@ -0,0 +1,10 @@
+const config = {
+    apiKey: "AIzaSyBZcPQo-Rv6RORqWGv_rcGSBEk8Ahygnbw",
+    authDomain: "wendy-webshop.firebaseapp.com",
+    projectId: "wendy-webshop",
+    storageBucket: "wendy-webshop.appspot.com",
+    messagingSenderId: "1098585740549",
+    appId: "1:1098585740549:web:90b8ac52807856a397d36c"
+  };
+  
+  export default config;
---
+++ wendy-webshop/src/firebase/firestore.js
@@ -0,0 +1,8 @@
+  
+import firebase from "firebase";
+import firebaseConfig from './config';
+import 'firebase/firestore';
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore;
