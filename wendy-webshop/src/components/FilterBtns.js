5b39a29f9f228bff2acd34b90f3ef23ac2bbd87b
Csilla <farkascsillamunka@gmail.com>
2021-04-13 20:32:19 +0200

    links fixed

--- wendy-webshop/src/components/FilterBtns.js
+++ wendy-webshop/src/components/FilterBtns.js
@@ -4,11 +4,11 @@ export default function FilterBtns() {
 
     return( 
         <div className="w-100 d-flex justify-content-between mt-3 mb-3">
-            <button className='btn btn-orange'><Link to='/only-available'>only available</Link></button>
-            <button className='btn btn-orange'><Link to='/cheapest-first'>cheapest first</Link></button>
-            <button className='btn btn-orange'><Link to='/contains-nike'>contains nike</Link></button>
-            <button className='btn btn-orange'><Link to='/average-stock'>average stock</Link></button>
-            <button className='btn btn-orange'><Link to='/most-expensive'>most expensive available</Link></button>
+            <Link to='/only-available'><button className='btn btn-orange'>only available</button></Link>
+            <Link to='/cheapest-first'><button className='btn btn-orange'>cheapest first</button></Link>
+            <Link to='/contains-nike'><button className='btn btn-orange'>contains nike</button></Link>
+            <Link to='/average-stock'><button className='btn btn-orange'>average stock</button></Link>
+            <Link to='/most-expensive'><button className='btn btn-orange'>most expensive available</button></Link>
         </div>
     )
 }
