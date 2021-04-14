001ddec52a1c93299babacfb7f3a7e4cd862fd1e
Edina Hajma-Korodi <66006617+hajmaedina@users.noreply.github.com>
2021-04-14 20:09:04 +0200

    update EditForm, add modify and delete buttons to Table, add Router to EditForm

--- wendy-webshop/src/components/EditForm.js
+++ wendy-webshop/src/components/EditForm.js
@@ -1,10 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { useState, useRef, useEffect } from 'react';
 // import InputFieldSet from './InputFieldSet';
-import db from './firebase/db';
+import db from '../firebase/db';
 
 export default function EditForm() {
-  const { id } = useParams();
+  // const { id } = useParams();
 
   const [fieldValues, setFieldValues] = useState({
     name: '',
@@ -73,7 +73,7 @@ export default function EditForm() {
 
   const errorTypes = {
     required: 'Hiányzó érték.',
-    oscars: 'Nem adható meg negatív szám.',
+    moreThanNull: 'Nem adható meg negatív szám.',
   };
 
   function isFormValid() {
@@ -141,45 +141,45 @@ export default function EditForm() {
   function handleSubmit(e) {
     e.preventDefault();
 
-    const isValid = isFormValid();
+    // const isValid = isFormValid();
 
-    if (isValid) {
-      db.collection('ShopItems')
-        .doc(id)
-        .update({
-          name: fieldValues.name,
-          type: fieldValues.type,
-          description: fieldValues.description,
-          price: parseInt(fieldValues.price),
-          quantityOfStock: parseInt(fieldValues.quantityOfStock),
-        })
-        .then((docRef) => {
-          setFieldValues({
-            name: '',
-            type: '',
-            description: '',
-            price: '',
-            quantityOfStock: '',
-          });
-          setFormAlertText('Sikeres módosítás.');
-          setFormAlertType('success');
-        });
-    } else {
-      setFormAlertText('Sikertelen módosítás.');
-      setFormAlertType('danger');
-    }
+    // if (isValid) {
+    //   db.collection('ShopItems')
+    //     .doc(id)
+    //     .update({
+    //       name: fieldValues.name,
+    //       type: fieldValues.type,
+    //       description: fieldValues.description,
+    //       price: parseInt(fieldValues.price),
+    //       quantityOfStock: parseInt(fieldValues.quantityOfStock),
+    //     })
+    //     .then((docRef) => {
+    //       setFieldValues({
+    //         name: '',
+    //         type: '',
+    //         description: '',
+    //         price: '',
+    //         quantityOfStock: '',
+    //       });
+    //       setFormAlertText('Sikeres módosítás.');
+    //       setFormAlertType('success');
+    //     });
+    // } else {
+    //   setFormAlertText('Sikertelen módosítás.');
+    //   setFormAlertType('danger');
+    // }
   }
 
-  useEffect(() => {
-    db.collection('ShopItems')
-      .doc(id)
-      .get()
-      .then((docRef) => {
-        let data = docRef.data();
+  // useEffect(() => {
+  //   db.collection('ShopItems')
+  //     .doc(id)
+  //     .get()
+  //     .then((docRef) => {
+  //       let data = docRef.data();
 
-        setFieldValues(data);
-      });
-  }, []);
+  //       setFieldValues(data);
+  //     });
+  // }, []);
 
   return (
     <div className="container">
--- wendy-webshop/src/components/Table.js
+++ wendy-webshop/src/components/Table.js
@@ -1,6 +1,9 @@
 import TableItem from './TableItem';
+import { Link } from 'react-router-dom';
+// import { Modal } from 'bootstrap';
 
 function Table({ products }) {
+  console.log('products:', products)
   return (
     <>
       <header className={'container mt-4 mb-4'}></header>
@@ -14,18 +17,39 @@ function Table({ products }) {
                 <th>Description</th>
                 <th>Price</th>
                 <th>Quantity of stock</th>
+                <th>Modify</th>
+                <th>Remove</th>
               </tr>
             </thead>
             <tbody>
               {products.map((product) => (
-                <TableItem
-                  key={product.id}
-                  name={product.name}
-                  type={product.type}
-                  description={product.description}
-                  price={product.price}
-                  quantityOfStock={product.quantityOfStock}
-                />
+                <tr key={product.docId}>
+                  <TableItem
+                    key={product.docId}
+                    name={product.name}
+                    type={product.type}
+                    description={product.description}
+                    price={product.price}
+                    quantityOfStock={product.quantityOfStock}
+                  />
+                  <td>
+                    <Link to={`/product/edit/${product.docId}`}>
+                    <button className="btn btn-primary me-3">Modify</button>
+                  </Link>
+                  </td>
+                  <td>
+                  <button
+                    className="btn btn-danger me-3"
+                    data-id={product.docId}
+                    data-bs-target="#myModal"
+                    data-bs-toggle="modal"
+                  // onClick={handleDeleteOnClick}
+                  >
+                    Remove
+                  </button>
+                  </td>
+                  
+                </tr>
               ))}
             </tbody>
           </table>
