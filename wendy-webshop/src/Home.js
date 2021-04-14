06e127bee2807387cd6eae4e3ebc79055cc8fb25
bagotaj <bagotaj@gmail.com>
2021-04-14 13:44:19 +0200

    Some basic modification

--- wendy-webshop/src/Home.js
+++ wendy-webshop/src/Home.js
@@ -4,15 +4,14 @@ import React, { useState, useEffect } from 'react';
 import db from './firebase/db';
 
 export default function Home() {
-
-
-return (
-    <h1>My Shop</h1>
+  return (
+    <>
+      <h1>My Shop</h1>
+    </>
     //Link buttons
     //Search
     //TableItems
     //NewProduct
     //Delete Modal
-    )   
+  );
 }
-
