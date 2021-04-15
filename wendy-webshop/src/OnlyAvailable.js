83d2350b76562f4ea8a959be5bb36ad7026683b5
Csilla <farkascsillamunka@gmail.com>
2021-04-15 19:58:20 +0200

    OnlyAvailable component was implemented in 2 version: need db call or not

--- wendy-webshop/src/OnlyAvailable.js
+++ wendy-webshop/src/OnlyAvailable.js
@@ -1,5 +1,38 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import Table from './components/Table';
+import db from './firebase/db';
 
-export default function OnlyAvailable() {
-  return <div>djgj</div>;
+
+export default function OnlyAvailable({products}) {
+
+  //Ha nem kell db lekeres, hanem hasznalhoto a products:
+  //const availableProducts = products.filter( product => product.quantityOfStock === 0 );
+
+  //Ha kell db lekeres:
+
+  const [ availableProducts, setAvailableProducts ] = useState([]);
+
+  useEffect( () => {
+    db.collection('shopItems')
+      .where('quantityOfStock', '==', 0 )
+      .get()
+      .then( ref => {
+        const data = [];
+        ref.docs.forEach( doc => {
+          const docItem = product.data();
+          docItem['docId'] = product.id;
+
+          data.push(docItem);
+        });
+
+        setAvailableProducts( data );
+      })
+      .catch( error => {
+        console.error( error.message );
+      })
+  }, []);
+
+  return (
+    <Table products={availableProducts} />
+  )
 }
