ae1e1ac1dde52a141252658d6663ad0dbe7bde2e
Adrián Nagy <nagy.adrian1986@gmail.com>
2021-04-15 09:24:03 +0200

    delete modal text: hungarian ==> english

--- wendy-webshop/src/components/DeleteItem.js
+++ wendy-webshop/src/components/DeleteItem.js
@@ -4,7 +4,7 @@ export default function DeleteItem({handleDeleteConfirm, deleteModalRef}) {
       <div className='modal-dialog'>
         <div className='modal-content'>
           <div className='modal-header'>
-            <h5 className='modal-title'>TÖRLÉS</h5>
+            <h5 className='modal-title'>REMOVE</h5>
             <button
               type='button'
               className='btn-close'
@@ -13,8 +13,8 @@ export default function DeleteItem({handleDeleteConfirm, deleteModalRef}) {
             ></button>
           </div>
           <div className='modal-body'>
-            <p>Biztosan törölni szeretné a kiválasztott terméket?</p>
-            <p>Vigyázat: A változásokat nem lehet visszavonni!</p>
+            <p>Are you sure to remove the product from the list?</p>
+            <p>Warning: Changes can not be undone!</p>
           </div>
           <div className='modal-footer'>
             <button
