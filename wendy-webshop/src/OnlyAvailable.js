c7f5fc8f312b84116eeb526ab219e87da49fab8e
bagotaj <bagotaj@gmail.com>
2021-04-15 21:13:13 +0200

    Bug fixing

--- wendy-webshop/src/OnlyAvailable.js
+++ wendy-webshop/src/OnlyAvailable.js
@@ -2,37 +2,33 @@ import { useEffect, useState } from 'react';
 import Table from './components/Table';
 import db from './firebase/db';
 
-
-export default function OnlyAvailable({products}) {
-
+export default function OnlyAvailable({ products }) {
   //Ha nem kell db lekeres, hanem hasznalhoto a products:
   //const availableProducts = products.filter( product => product.quantityOfStock === 0 );
 
   //Ha kell db lekeres:
 
-  const [ availableProducts, setAvailableProducts ] = useState([]);
+  const [availableProducts, setAvailableProducts] = useState([]);
 
-  useEffect( () => {
+  useEffect(() => {
     db.collection('shopItems')
-      .where('quantityOfStock', '==', 0 )
+      .where('quantityOfStock', '==', 0)
       .get()
-      .then( ref => {
+      .then((ref) => {
         const data = [];
-        ref.docs.forEach( doc => {
-          const docItem = product.data();
-          docItem['docId'] = product.id;
+        ref.docs.forEach((doc) => {
+          const docItem = doc.data();
+          docItem['docId'] = doc.id;
 
           data.push(docItem);
         });
 
-        setAvailableProducts( data );
+        setAvailableProducts(data);
       })
-      .catch( error => {
-        console.error( error.message );
-      })
+      .catch((error) => {
+        console.error(error.message);
+      });
   }, []);
 
-  return (
-    <Table products={availableProducts} />
-  )
+  return <Table products={availableProducts} />;
 }
--- wendy-webshop/src/firebase/config.js
+++ wendy-webshop/src/firebase/config.js
@@ -1,19 +1,19 @@
-const config = {
-    apiKey: "AIzaSyBZcPQo-Rv6RORqWGv_rcGSBEk8Ahygnbw",
-    authDomain: "wendy-webshop.firebaseapp.com",
-    projectId: "wendy-webshop",
-    storageBucket: "wendy-webshop.appspot.com",
-    messagingSenderId: "1098585740549",
-    appId: "1:1098585740549:web:90b8ac52807856a397d36c"
-  };
+// const config = {
+//     apiKey: "AIzaSyBZcPQo-Rv6RORqWGv_rcGSBEk8Ahygnbw",
+//     authDomain: "wendy-webshop.firebaseapp.com",
+//     projectId: "wendy-webshop",
+//     storageBucket: "wendy-webshop.appspot.com",
+//     messagingSenderId: "1098585740549",
+//     appId: "1:1098585740549:web:90b8ac52807856a397d36c"
+//   };
 
-  // const config = {
-  //   apiKey: "AIzaSyCmK0OxZ7j7jvIcwc2s9jHL2w6kXN1nzrw",
-  //   authDomain: "form-app02.firebaseapp.com",
-  //   projectId: "form-app02",
-  //   storageBucket: "form-app02.appspot.com",
-  //   messagingSenderId: "890415884102",
-  //   appId: "1:890415884102:web:72583769d11f1bfad22747"
-  // }
-  
-  export default config;
+const config = {
+  apiKey: 'AIzaSyCmK0OxZ7j7jvIcwc2s9jHL2w6kXN1nzrw',
+  authDomain: 'form-app02.firebaseapp.com',
+  projectId: 'form-app02',
+  storageBucket: 'form-app02.appspot.com',
+  messagingSenderId: '890415884102',
+  appId: '1:890415884102:web:72583769d11f1bfad22747',
+};
+
+export default config;
