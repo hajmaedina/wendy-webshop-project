1ea8c5860b0c619ad61115c1ab7265d9d7317597
Adrián Nagy <nagy.adrian1986@gmail.com>
2021-04-16 05:50:16 +0200

    Revert "filtering Nike products implemented"
    This reverts commit d474e0b14a8777cbd58de244c2a43dc85edd135e.
    
    because i fucked up everyone's work

--- wendy-webshop/src/ContainsNike.js
+++ wendy-webshop/src/ContainsNike.js
@@ -1,54 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
-
-import db from './firebase/db';
- 
-import NavBar from './components/NavBar';
-import Table from './components/Table';
+import React from 'react';
 
 export default function ContainsNike() {
-  const [links, setLinks] = useState({
-    'only available': '/only-available',
-    'cheapest first': '/cheapest-first',
-    'contains nike': '/contains-nike',
-    'average stock': '/average-stock',
-    'most expensive available': '/most-expensive',
-  });
-
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const unsubscribe = db.collection('shopItems').onSnapshot((snapshot) => {
-      const data = [];
-
-      snapshot.docs.forEach((product) => {
-        const docItem = product.data();
-        docItem['docId'] = product.id;
-
-        data.push(docItem);
-      });
-      setProducts(data);
-    });
-
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-  
-  const nikeProducts = products.filter(
-      product => 
-      product.description.toLowerCase().includes('nike') 
-      || product.name.toLowerCase().includes('nike')
-  );
-
-  return (
-    <div className='container'>
-      <Link to="/" className="text-decoration-none">
-      <h1 className="text-info mt-3">My Shop</h1>
-      </Link>
-      <hr className='text-info' />
-      <NavBar links={links} />
-      <Table products={nikeProducts} />
-    </div>
-  );
+  return <div>rhgkgl</div>;
 }
