b6ee7fc8d046512a9d1fc53eb53e52dd20bd3c07
Csilla <farkascsillamunka@gmail.com>
2021-04-19 15:43:39 +0200

    db call added, filter works

--- wendy-webshop/src/components/FilterByPrice.js
+++ wendy-webshop/src/components/FilterByPrice.js
@@ -1,30 +1,40 @@
 import PriceInput from './PriceInput';
 import { useState } from 'react';
-//import db from '../firebase/db';
+import db from '../firebase/db';
 
-export default function FilterByPrice({products, setProducts}) {
+export default function FilterByPrice({ products, setProducts }) {
 
-    const [price, setPrice] = useState('');
+  const [price, setPrice] = useState('');
 
-    function handleInputOnChange(e) {
-      setPrice(e.target.value);
-    }
+  function handleInputOnChange(e) {
+    setPrice(e.target.value);
+  }
 
-    function handleFilterByPrice(e) {
-      const filterFrom = e.target.dataset.filter;
-      
-      console.log ( filterFrom + price );
+  function handleFilterByPrice(e) {
+    const filterFrom = e.target.dataset.filter;
 
-      //db call:
-      // db.collection( 'shopItems' )
-      //   .where('price', filterFrom, price)
-      //   .get()
-      //   .then(...setProducts())
-      
-    }
+    db.collection('shopItems')
+      .where('price', filterFrom, parseInt(price))
+      .get()
+      .then(ref => {
+        const data = [];
 
-    return(
-        <>
+        ref.docs.forEach((product) => {
+          const docItem = product.data();
+          docItem['docId'] = product.id;
+
+          data.push(docItem);
+        });
+        setProducts(data);
+      })
+      .catch( error => {
+        console.error( error.message );
+      })
+
+  }
+
+  return (
+    <>
       <div className="w-100 d-flex align-items-end">
         <button
           className="btn btn-orange h-50 w-25 me-3"
@@ -59,5 +69,5 @@ export default function FilterByPrice({products, setProducts}) {
         </div>
       </div>
     </>
-    )
+  )
 }
